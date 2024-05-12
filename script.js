21bca0cfad1ce6eb9ed40ef00c341e6e027c8b39
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-05-12 13:14:35 +0200

    We have image carousel

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//All related to dropdown menu
+
 const buttonDrop = document.querySelector(".dropbtn");
 
 buttonDrop.addEventListener("click", () =>
@@ -14,3 +16,34 @@ window.addEventListener("click", (e) =>
 			drowpdown[i].classList.remove("show");
 	}
 });
+
+
+//All related to image carousel
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+document.querySelector(".prev").addEventListener("click", () => plusSlides(-1));
+document.querySelector(".next").addEventListener("click", () => plusSlides(1));
+
+function plusSlides(n)
+{
+	showSlides(slideIndex += n);
+}
+
+function showSlides(n)
+{
+	let i;
+	const slides = document.getElementsByClassName("slide-img");
+	const dots = document.getElementsByClassName("dot");
+	if (n > slides.length)
+		slideIndex = 1;
+	if (n < 1)
+		slideIndex = slides.length;
+	for (i = 0; i < slides.length; i++)
+		slides[i].style.display = "none";
+	for (i = 0; i < dots.length; i++)
+		dots[i].className = dots[i].className.replace(" active", "");
+	slides[slideIndex - 1].style.display = "block";
+	dots[slideIndex - 1].className += " active";
+}
